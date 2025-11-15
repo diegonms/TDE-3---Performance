a68c28dd3d3fa3be46518576aac63516fd42a04d
peratop <p.leonardo7k@gmail.com>
2025-11-14 21:59:24 -0300

    deadlock criado

---
+++ Deadlock/src/main/java/br/pucpr/DeadlockResolvido.java
@@ -0,0 +1,48 @@
+package br.pucpr;
+
+public class DeadlockResolvido {
+    static final Object LOCK_A = new Object();
+    static final Object LOCK_B = new Object();
+
+    public static void main(String[] args) {
+
+        Thread t1 = new Thread(() -> {
+            System.out.println("T1: tenta pegar LOCK_A");
+            synchronized (LOCK_A) {
+                System.out.println("T1: pegou LOCK_A");
+                dormir(100);
+
+                System.out.println("T1: tentou pegar LOCK_B");
+                synchronized (LOCK_B) {
+                    System.out.println("T1 concluiu");
+                }
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            System.out.println("T2: tenta pegar LOCK_B");
+            synchronized (LOCK_B) {
+                System.out.println("T2: pegou LOCK_B");
+                dormir(100);
+
+                System.out.println("T2: tentou pegar LOCK_A");
+                synchronized (LOCK_A) {
+                    System.out.println("T2 concluiu");
+                }
+            }
+        });
+
+        t1.start();
+        t2.start();
+    }
+
+    // sleep para o codigo não rodar instantanemente
+    // impede que o T1 pegue os dois locks antes do T2 acordar
+    static void dormir(long ms) {
+        try {
+            Thread.sleep(ms);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+}
